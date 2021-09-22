ab1bd75d5a926e3e821a8b827bd9d26675cdaa37
Kofi Adutwum <kadut001@odu.edu>
2021-09-22 11:11:35 -0400

    still working on it

--- iLab/App.js
+++ iLab/App.js
@@ -1,13 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+import {Agenda, Calendar, LocaleConfig} from 'react-native-calendars'
+
+const Item = {
+  '2021-09-22': [{room: "BAB 1003", time: "10:30 PM"}],
+  '2021-09-23': [{room: "BAB 2011", time: "5:30 PM"}]
+}
 
 export default function App() {
+  
+  const renderItem = () => {
+   return (
+      <View style={styles.itemContainer}>
+        <Text>{"te"}</Text>
+      </View>
+   )
+  }
+  const empty = () => {
+    return (
+      <View>
+        <Text>No Schedule</Text>
+      </View>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Agenda 
+        items={Item}
+        // renderDay={renderItem}
+        
+        renderItem={renderItem}
+        // renderEmptyDate={empty}
+        renderEmptyData={empty}
+        showClosingKnob={true}
+        
+        // selected={'2021-09-01'}
+        // onDayPress={(day) => {console.log(day)}}
+        // onDayChange={(day) => {console.log('day changed')}}
+        theme={{
+          agendaKnobColor: 'black',
+          agendaTodayColor: 'red',
+        }}
+
+        style={{
+          backgroundColor: "red",
+        }}
+        
+      />
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +58,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  
+  },
+  itemContainer: {
+    backgroundColor: 'white',
+    margin: 9,
+    borderRadius: 15,
     justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+    height: 200,
   },
 });
